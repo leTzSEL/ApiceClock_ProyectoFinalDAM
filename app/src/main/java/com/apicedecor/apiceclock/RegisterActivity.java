98020f9684c093aa6604e25a67c1871e7aa9a915
Letizia <letizia4enriquez@gmail.com>
2025-06-02 22:03:35 +0200

    cambios quitan imports no funcionales

--- app/src/main/java/com/apicedecor/apiceclock/RegisterActivity.java
+++ app/src/main/java/com/apicedecor/apiceclock/RegisterActivity.java
@@ -12,7 +12,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
